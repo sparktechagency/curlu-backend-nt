ed6299917a70be0adb06d9a335d9e7002e33aa72
MdJusef <mdjusef143@gmail.com>
2024-08-22 16:20:03 +0600

    manage event

--- app/Http/Controllers/SuperAdminDashboard/EShop/ProductController.php
+++ app/Http/Controllers/SuperAdminDashboard/EShop/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\SuperAdminDashboard\Eshop;
+namespace App\Http\Controllers\SuperAdminDashboard\EShop;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductRequest;
